691c0cb2c97ecd99fd1fdcc1dda28cd8c84b296c
karthikshettyks26 <karthikshettyks26@gmail.com>
2024-08-04 23:02:31 +0530

    Created Image model

--- INWalks.APIs/Data/INWalksDbContext.cs
+++ INWalks.APIs/Data/INWalksDbContext.cs
@@ -15,6 +15,7 @@ namespace INWalks.API.Data
         public DbSet<Difficulty> Difficulties { get; set; }
         public DbSet<Region> Regions { get; set; }
         public DbSet<Walk> Walks { get; set; }
+        public DbSet<Image> Images { get; set; }
 
         //Seed Database
 
